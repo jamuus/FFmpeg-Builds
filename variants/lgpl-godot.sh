776ed6f02c1ddb691e9cc5298c47ab7e6314d7a8
Jam <jamessewart@gmail.com>
2023-09-15 18:22:34 +0100

    enable mpegts but informed

--- variants/lgpl-godot.sh
+++ variants/lgpl-godot.sh
@@ -2,7 +2,7 @@ FF_CONFIGURE="$FF_CONFIGURE --disable-hwaccels --disable-filters --disable-progr
 VORBIS="--enable-libvorbis --enable-parser=vorbis --enable-decoder=vorbis --enable-decoder=libvorbis"
 VP9="--enable-demuxer=matroska --enable-decoder=vp9 --enable-parser=vp9"
 VP8="--enable-decoder=vp8 --enable-parser=vp8"
-H264="--enable-decoder=mpeg4,h264,aac,aac_latm,mp3,mpegts --enable-demuxer=mov,aac,flv,avi --enable-parser=h264,mpeg4video,mpegaudio,mpegvideo,aac,aac_latm,mpegts"
+H264="--enable-decoder=mpeg4,h264,aac,aac_latm,mp3 --enable-demuxer=mov,aac,flv,avi,mpegts,mpegtsraw --enable-parser=h264,mpeg4video,mpegaudio,mpegvideo,aac,aac_latm"
 OPUS="--enable-libopus --enable-parser=opus --enable-decoder=opus --enable-decoder=libopus"
 
 FF_CONFIGURE="$FF_CONFIGURE $VORBIS $VP9 $VP8 $H264 $OPUS"
